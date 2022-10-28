6d43e0fc2d742dd5c3e6299f62e8f9600d040974
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-28 13:17:11 +0200

    Refactor Code

--- Problem1_#217_Contains Duplicate/bruteForceLoopSol.py
+++ Problem1_#217_Contains Duplicate/bruteForceLoopSol.py
@@ -1,27 +1,29 @@
 
 # Solution using Brute Force Algorithm
 # Time Complexity worst case O(n^2)
-
+# LeetCode Runtime Result is around 60 ms
 
 from typing import List
 
-test = [1,2,3,4,5,6,7,8,9,10,100,15,-5,5]
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 15, -5, 5]
+
 
 class Solution:
 
     def containsDuplicate(self, nums: List[int]) -> bool:
-         
+
         new_list = []
-        
-        for i in nums :
-            if not new_list :
+
+        for i in nums:
+            if not new_list:
                 new_list.append(i)
-            else :
-                for x in new_list :
-                    if i == x :
+            else:
+                for x in new_list:
+                    if i == x:
                         return True
-                new_list.append(i)        
+                new_list.append(i)
         return False
+
 
 if __name__ == "__main__":
     Test = Solution()
