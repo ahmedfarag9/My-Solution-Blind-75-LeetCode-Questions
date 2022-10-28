5b1f1adef9413f01c74aeed0a005ad49d539e2e7
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-28 13:20:56 +0200

    Add mergeSortSol initial code

---
+++ Problem1_#217_Contains Duplicate/mergeSortReturnAdjacentSol.py
@@ -0,0 +1,17 @@
+
+# Solution using Merge sort then searching for adjacent elements
+# Time Complexity worst case O(nlog(n))
+# LeetCode Runtime Result is around 40 ms
+
+from typing import List
+
+
+class Solution:
+
+if __name__ == "__main__":
+
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 15, -5]
+    #array = [1, 2, 3, 1]
+
+    Test = Solution()
+    Test.mergeSort(array)
