31fd75952f0f6981cea6f051ae8a4b1256552bfa
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-01 16:34:43 +0200

    Add HashFuncSol.py

---
+++ Problem1_#217_Contains Duplicate/HashingFuncSol.py
@@ -0,0 +1,19 @@
+# Solution using Brute Force Algorithm
+# Time Complexity worst case O(n)
+# Space Complexity worst case O(n)
+
+
+from typing import List
+
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 15, -5]
+
+
+class Solution:
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return False
+
+
+if __name__ == "__main__":
+    Test = Solution()
+    print(Test.containsDuplicate(test))
