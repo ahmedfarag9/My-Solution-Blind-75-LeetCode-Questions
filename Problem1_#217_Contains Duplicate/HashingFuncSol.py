f46c2ca97875b5784dfa5ab28e688d06e766a6b6
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-01 16:35:19 +0200

    Add Hash set implementation

--- Problem1_#217_Contains Duplicate/HashingFuncSol.py
+++ Problem1_#217_Contains Duplicate/HashingFuncSol.py
@@ -11,6 +11,14 @@ test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 15, -5]
 class Solution:
 
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Create Hash set
+        hash_set = set()
+
+        # Check if set contains input list elements
+        for i in nums:
+            if i in hash_set:
+                return True
+            hash_set.add(i)
         return False
 
 
