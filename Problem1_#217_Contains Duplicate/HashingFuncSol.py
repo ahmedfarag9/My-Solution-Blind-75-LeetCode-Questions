f3d81110aa6e7efae348beb05734d78c47032da2
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-04 14:15:03 +0200

    Add comment

--- Problem1_#217_Contains Duplicate/HashingFuncSol.py
+++ Problem1_#217_Contains Duplicate/HashingFuncSol.py
@@ -1,4 +1,7 @@
-# Solution using Brute Force Algorithm
+# Problem 217. Contains Duplicate
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Solution using Hash Function
 # Time Complexity worst case O(n)
 # Space Complexity worst case O(n)
 
