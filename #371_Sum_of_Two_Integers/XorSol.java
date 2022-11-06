63c6a3555c28214e518463667ff4892b986a0179
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-06 19:36:54 +0200

    Add describtion comments

---
+++ #371_Sum_of_Two_Integers/XorSol.java
@@ -0,0 +1,11 @@
+// Problem 371. Sum of Two Integers
+// Given two integers a and b, return the sum of the two integers without using the operators + and -
+
+// Solution using XOR logic operations
+// Time Complexity worst case O(n)
+// Space Complexity worst case O(n)
+
+// LeetCode Results:
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Sum of Two Integers.
+// Memory Usage: 41.2 MB, less than 20.61% of Java online submissions for Sum of Two Integers.
+
